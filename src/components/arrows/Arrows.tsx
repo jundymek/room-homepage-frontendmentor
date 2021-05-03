86c8edc8dd0babbd67393d4355db581b296de108
jundymek <jundymek@gmail.com>
2021-05-03 23:28:01 +0200

    style: Navigation links, Arrows sizes on mobile

--- src/components/arrows/Arrows.tsx
+++ src/components/arrows/Arrows.tsx
@@ -3,17 +3,21 @@ import styles from "./Arrows.module.scss";
 import arrowLeft from "../../images/icon-angle-left.svg";
 import arrowRight from "../../images/icon-angle-right.svg";
 import { useSliderImage } from "../../context/slidersContext";
+import { useWindowWidth } from "../../customHooks/useWindowWidth";
 
 const Arrows = () => {
   const { handleChangeSlide } = useSliderImage();
+  const windowWidth = useWindowWidth();
+  const arrowWidth = windowWidth < 1000 ? "10" : "15";
+  const arrowHeight = windowWidth < 1000 ? "15" : "25";
 
   return (
     <div className={styles.arrows}>
       <button className={styles.button} onClick={() => handleChangeSlide("left")}>
-        <img src={arrowLeft} alt="" />
+        <img src={arrowLeft} alt="" className="arrow" width={arrowWidth} height={arrowHeight} />
       </button>
       <button className={styles.button} onClick={() => handleChangeSlide("right")}>
-        <img src={arrowRight} alt="" />
+        <img src={arrowRight} alt="" width={arrowWidth} height={arrowHeight} />
       </button>
     </div>
   );
